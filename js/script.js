ac188b8c5d4d3976052fff282f473a374880bc36
Giphty Mensah <giftym870@gmail.com>
2022-03-10 20:15:52 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let firstName = "Theo";
+let lastName = "Ruth";
+let age = 21;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+console.log("My name is " + firstName + " " + lastName + ". I am " + age + " years old. ");
+
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old.`);
+
+let fruits = [ "Eba", "apple", "coconut", "sugarcane", "orange"];
+console.log(fruits)
+console.log(fruits.length);
+console.log(fruits [0]);
+console.log(fruits.push("banana"));
+console.log(fruits);
+console.log(fruits.pop());
+console.log(fruits.pop());
+console.log(fruits)
+console.log(fruits.unshift("pear"));
+console.log(fruits);
+console.log(fruits.shift("pear"));
+console.log(fruits)
+console.log(fruits.includes("Eba"));
+console.log(fruits.includes("ginger"));
+console.log(fruits.join("-"))
